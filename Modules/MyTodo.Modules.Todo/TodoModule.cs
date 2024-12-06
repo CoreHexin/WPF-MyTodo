7a6cf1d32ae872324c8828b799014b8e8420be52
Hexin <hexin.email@qq.com>
2024-12-06 20:43:36 +0800

    添加待办事项模块

---
+++ Modules/MyTodo.Modules.Todo/TodoModule.cs
@@ -0,0 +1,21 @@
+﻿using MyTodo.Modules.Todo.ViewModels;
+using MyTodo.Modules.Todo.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace MyTodo.Modules.Todo
+{
+    public class TodoModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<TodoView, TodoViewModel>();
+        }
+    }
+}
