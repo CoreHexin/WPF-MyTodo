36ad64b85d2fc67997703edb626eb3148126bc54
Hexin <hexin.email@qq.com>
2024-12-06 21:17:31 +0800

    添加设置模块

---
+++ Modules/MyTodo.Modules.Settings/SettingsModule.cs
@@ -0,0 +1,21 @@
+﻿using MyTodo.Modules.Settings.ViewModels;
+using MyTodo.Modules.Settings.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace MyTodo.Modules.Settings
+{
+    public class SettingsModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<SettingsView, SettingsViewModel>();
+        }
+    }
+}
