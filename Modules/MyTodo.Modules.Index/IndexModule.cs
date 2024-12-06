b08416ac2239f5c2c94e063ef08a54ae17aee7f2
Hexin <hexin.email@qq.com>
2024-12-06 20:43:16 +0800

    添加首页模块

---
+++ Modules/MyTodo.Modules.Index/IndexModule.cs
@@ -0,0 +1,21 @@
+﻿using MyTodo.Modules.Index.ViewModels;
+using MyTodo.Modules.Index.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace MyTodo.Modules.Index
+{
+    public class IndexModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<IndexView, IndexViewModel>();
+        }
+    }
+}
---
+++ Modules/MyTodo.Modules.Index/Views/IndexView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MyTodo.Modules.Index.Views
+{
+    /// <summary>
+    /// Interaction logic for IndexView
+    /// </summary>
+    public partial class IndexView : UserControl
+    {
+        public IndexView()
+        {
+            InitializeComponent();
+        }
+    }
+}
