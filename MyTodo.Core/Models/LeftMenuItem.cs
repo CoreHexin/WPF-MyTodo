f5a4995613c485242bd9380dbbd207e001664a15
Hexin <hexin.email@qq.com>
2024-12-06 18:16:10 +0800

    左侧菜单栏界面设计

---
+++ MyTodo.Core/Models/LeftMenuItem.cs
@@ -0,0 +1,14 @@
+﻿namespace MyTodo.Core.Models
+{
+    public class LeftMenuItem
+    {
+        // 菜单icon
+        public string Icon { get; set; }
+
+        // 菜单名称
+        public string Name { get; set; }
+
+        // 视图名称
+        public string ViewName { get; set; }
+    }
+}
--- MyTodo/ViewModels/MainWindowViewModel.cs
+++ MyTodo/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,57 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.Generic;
+using MyTodo.Core.Models;
+using Prism.Mvvm;
 
 namespace MyTodo.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
-        public string Title
+        public List<LeftMenuItem> LeftMenuItems { get; set; }
+
+        private LeftMenuItem _selectedMenu;
+        public LeftMenuItem SelectedMenu
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get { return _selectedMenu; }
+            set { SetProperty(ref _selectedMenu, value); }
         }
 
         public MainWindowViewModel()
         {
+            InitLeftMenu();
+        }
 
+        /// <summary>
+        /// 初始化左侧菜单栏
+        /// </summary>
+        private void InitLeftMenu()
+        {
+            LeftMenuItems = new List<LeftMenuItem>()
+            {
+                new LeftMenuItem()
+                {
+                    Icon = "Home",
+                    Name = "首页",
+                    ViewName = "IndexView",
+                },
+                new LeftMenuItem()
+                {
+                    Icon = "NotebookOutline",
+                    Name = "待办事项",
+                    ViewName = "TodoView",
+                },
+                new LeftMenuItem()
+                {
+                    Icon = "NotebookEditOutline",
+                    Name = "备忘录",
+                    ViewName = "MemoView",
+                },
+                new LeftMenuItem()
+                {
+                    Icon = "Cog",
+                    Name = "设置",
+                    ViewName = "SettingsView",
+                },
+            };
         }
     }
 }
