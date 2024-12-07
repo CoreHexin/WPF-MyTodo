0c1a8bee9485ee4e889fe59e4eca5b218771573f
Hexin <hexin.email@qq.com>
2024-12-07 13:36:19 +0800

    首页统计面板界面设计

--- Modules/MyTodo.Modules.Index/ViewModels/IndexViewModel.cs
+++ Modules/MyTodo.Modules.Index/ViewModels/IndexViewModel.cs
@@ -1,16 +1,60 @@
-﻿using Prism.Commands;
+﻿using MyTodo.Core.Models;
 using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.ObjectModel;
 
 namespace MyTodo.Modules.Index.ViewModels
 {
     public class IndexViewModel : BindableBase
     {
+        private ObservableCollection<StatisticPanel> _statisticPanels;
+        public ObservableCollection<StatisticPanel> StatisticPanels
+        {
+            get { return _statisticPanels; }
+            set { SetProperty(ref _statisticPanels, value); }
+        }
+
         public IndexViewModel()
         {
+            CreateStatisticPanels();
+        }
 
+        private void CreateStatisticPanels()
+        {
+            StatisticPanels = new ObservableCollection<StatisticPanel>()
+            {
+                new StatisticPanel()
+                {
+                    Icon = "ClockFast",
+                    Title = "汇总",
+                    Content = "10",
+                    Background = "#FF0CA0FF",
+                    Target = "",
+                },
+                new StatisticPanel()
+                {
+                    Icon = "ClockCheckOutline",
+                    Title = "已完成",
+                    Content = "10",
+                    Background = "#FF1ECA3A",
+                    Target = "",
+                },
+                new StatisticPanel()
+                {
+                    Icon = "ChartLineVariant",
+                    Title = "完成率",
+                    Content = "80%",
+                    Background = "#FF02C6DC",
+                    Target = "",
+                },
+                new StatisticPanel()
+                {
+                    Icon = "PlaylistStar",
+                    Title = "备忘录",
+                    Content = "8",
+                    Background = "#FFFFA000",
+                    Target = "",
+                },
+            };
         }
     }
 }
---
+++ MyTodo.Core/Models/StatisticPanel.cs
@@ -0,0 +1,15 @@
+﻿namespace MyTodo.Core.Models
+{
+    public class StatisticPanel
+    {
+        public string Icon { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Background { get; set; }
+
+        public string Target { get; set; }
+    }
+}
