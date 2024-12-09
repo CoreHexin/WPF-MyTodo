c228e44eb06dc4abe8307c3e01e384f27d5c9d5a
Hexin <hexin.email@qq.com>
2024-12-09 23:09:02 +0800

    待办事项页面设计

--- Modules/MyTodo.Modules.Todo/ViewModels/TodoViewModel.cs
+++ Modules/MyTodo.Modules.Todo/ViewModels/TodoViewModel.cs
@@ -1,16 +1,38 @@
-﻿using Prism.Commands;
+﻿using System.Collections.ObjectModel;
+using MyTodo.Core.Models;
 using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace MyTodo.Modules.Todo.ViewModels
 {
     public class TodoViewModel : BindableBase
     {
+        private ObservableCollection<TodoItem> _todoItems;
+        public ObservableCollection<TodoItem> TodoItems
+        {
+            get { return _todoItems; }
+            set { SetProperty(ref _todoItems, value); }
+        }
+
         public TodoViewModel()
         {
+            CreateTodoItems();
+        }
 
+        private void CreateTodoItems()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                TodoItems.Add(
+                    new TodoItem()
+                    {
+                        Id = i,
+                        Title = $"待办事项标题{i}",
+                        Content = $"待办事项内容{i}",
+                    }
+                );
+            }
         }
     }
 }
