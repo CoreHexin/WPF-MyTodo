2b032d2cccd7dd8ab8ce46c07d922a5bad097515
Hexin <hexin.email@qq.com>
2024-12-10 22:37:04 +0800

    新增切换浅色和深色主题功能

--- Modules/MyTodo.Modules.Settings/ViewModels/CustomizationViewModel.cs
+++ Modules/MyTodo.Modules.Settings/ViewModels/CustomizationViewModel.cs
@@ -1,16 +1,42 @@
-﻿using Prism.Commands;
+﻿using System;
+using MaterialDesignThemes.Wpf;
 using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace MyTodo.Modules.Settings.ViewModels
 {
     public class CustomizationViewModel : BindableBase
     {
+        private bool _isDarkTheme;
+        public bool IsDarkTheme
+        {
+            get => _isDarkTheme;
+            set
+            {
+                if (SetProperty(ref _isDarkTheme, value))
+                {
+                    ModifyTheme(theme =>
+                        theme.SetBaseTheme(value ? BaseTheme.Dark : BaseTheme.Light)
+                    );
+                }
+            }
+        }
+
         public CustomizationViewModel()
         {
+            PaletteHelper paletteHelper = new PaletteHelper();
+            Theme theme = paletteHelper.GetTheme();
 
+            IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark;
+        }
+
+        private static void ModifyTheme(Action<Theme> modificationAction)
+        {
+            var paletteHelper = new PaletteHelper();
+            Theme theme = paletteHelper.GetTheme();
+
+            modificationAction?.Invoke(theme);
+
+            paletteHelper.SetTheme(theme);
         }
     }
 }
