745617a22a6c8658d100b75ee84373a75b7cd18b
Hexin <hexin.email@qq.com>
2024-12-12 22:25:05 +0800

    首页ToggleButton显示待办事项完成状态

---
+++ MyTodo.Core/Converters/BaseConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace MyTodo.Core.Converters
+{
+    public abstract class BaseConverter<T> : MarkupExtension, IValueConverter
+        where T : class, new()
+    {
+        private static T? _converter;
+
+        public abstract object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture
+        );
+
+        public abstract object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture
+        );
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _converter ?? (_converter = new T());
+        }
+    }
+}
---
+++ MyTodo.Core/Converters/IntToBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace MyTodo.Core.Converters
+{
+    public class IntToBoolConverter : BaseConverter<IntToBoolConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int)value == 1)
+                return true;
+            return false;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
