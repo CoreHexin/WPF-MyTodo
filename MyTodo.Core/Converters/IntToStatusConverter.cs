79832b9b3cf0897ec351bb5bb386737c7f9a31c7
Hexin <hexin.email@qq.com>
2024-12-16 21:21:04 +0800

    待办事项页面显示待办状态

---
+++ MyTodo.Core/Converters/IntToStatusConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace MyTodo.Core.Converters
+{
+    public class IntToStatusConverter : BaseConverter<IntToStatusConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int)value == 0)
+                return "[待办]";
+            return "[已完成]";
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
